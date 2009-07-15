760249a5c5d87083b2fa49c3c16e68cdf259e707
Mitar <mitar.git@tnode.com>
2009-07-15 13:11:27 +0000

    Added homepage files. #114

---
+++ htdocs/jquery.visage.init.js
@@ -0,0 +1,18 @@
+$.fn.visage.defaults.files = {
+	"blank": "/chrome/site/blank.gif",
+	"error": "/chrome/site/error.png"
+};
+$.fn.visage.defaults.attr.image.src = "/chrome/site/blank.gif";
+$.fn.visage.defaults.addDOM = function (visageDOM, options) {
+	$.fn.visage.addDOM(visageDOM, options);
+	// Moves elements to overlay so they are all in the same stacking context
+	$(visageDOM.prev).add(visageDOM.next).add(visageDOM.count).add(visageDOM.title).appendTo(visageDOM.overlay);
+};
+$.fn.visage.defaults.imageValues = function (image, options) {
+	var values = $.fn.visage.imageValues(image, options);
+	values.src = values.src.replace(/^\/attachment\//, "/raw-attachment/");
+	return values;
+};
+$(document).ready(function () {
+	$("a:has(img)").visage();
+});
