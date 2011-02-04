f6dbf61d2efa21bf5bf27205c0ce83c7ce3dbc0e
Mitar <mitar.git@tnode.com>
2011-02-04 13:45:39 +0100

    Initial installation with some basic configuration.

---
+++ settings.py
@@ -0,0 +1,179 @@
+# -*- coding: utf-8 -*-
+# Django settings for mainpage project.
+
+import os
+
+_ = lambda s: s
+PROJECT_PATH = os.path.abspath(os.path.dirname(__file__))
+
+DEBUG = True
+TEMPLATE_DEBUG = DEBUG
+
+ADMINS = (
+    ('Admin', 'webmaster@orange.biolab.si'),
+)
+
+MANAGERS = ADMINS
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': 'biolab',                                   # Or path to database file if using sqlite3.
+        'USER': 'django',                                   # Not used with sqlite3.
+        'PASSWORD': '',                                     # Not used with sqlite3.
+        'HOST': '',                                         # Set to empty string for localhost. Not used with sqlite3.
+        'PORT': '',                                         # Set to empty string for default. Not used with sqlite3.
+    }
+}
+
+# Local time zone for this installation. Choices can be found here:
+# http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
+# although not all choices may be available on all operating systems.
+# On Unix systems, a value of None will cause Django to use the same
+# timezone as the operating system.
+# If running in a Windows environment this must be set to the same as your
+# system time zone.
+TIME_ZONE = 'Europe/Ljubljana'
+
+# Language code for this installation. All choices can be found here:
+# http://www.i18nguy.com/unicode/language-identifiers.html
+LANGUAGE_CODE = 'en'
+
+LANGUAGES = (
+    ('en', _('English')),
+)
+
+URL_VALIDATOR_USER_AGENT = 'Django'
+
+# Date input formats below take as first argument day and then month in x/y/z format
+DATE_INPUT_FORMATS = (
+    '%Y-%m-%d', '%d/%m/%Y', '%d/%m/%y', '%b %d %Y',
+    '%b %d, %Y', '%d %b %Y', '%d %b, %Y', '%B %d %Y',
+    '%B %d, %Y', '%d %B %Y', '%d %B, %Y',
+)
+
+# All those formats are only defaults and are localized for users
+DATE_FORMAT = 'd/M/Y'
+TIME_FORMAT = 'H:i'
+DATETIME_FORMAT = 'd/M/Y, H:i'
+YEAR_MONTH_FORMAT = 'F Y'
+MONTH_DAY_FORMAT = 'j F'
+SHORT_DATE_FORMAT = 'd/m/y'
+SHORT_DATETIME_FORMAT = 'd/m/y H:i'
+FIRST_DAY_OF_WEEK = 1
+
+LOGIN_REDIRECT_URL = '/admin/'
+LOGIN_URL = '/admin/'
+LOGOUT_URL = '/admin/'
+
+FORCE_SCRIPT_NAME = ''
+
+SITE_ID = 1
+
+# If you set this to False, Django will make some optimizations so as not
+# to load the internationalization machinery.
+USE_I18N = True
+
+# If you set this to False, Django will not format dates, numbers and
+# calendars according to the current locale
+USE_L10N = True
+
+# Absolute filesystem path to the directory that will hold user-uploaded files.
+# Example: "/home/media/media.lawrence.com/"
+MEDIA_ROOT = os.path.join(PROJECT_PATH, "media")
+
+# URL that handles the media served from MEDIA_ROOT. Make sure to use a
+# trailing slash if there is a path component (optional in other cases).
+# Examples: "http://media.lawrence.com", "http://example.com/media/"
+MEDIA_URL = '/media/'
+
+# URL prefix for admin media -- CSS, JavaScript and images. Make sure to use a
+# trailing slash.
+# Examples: "http://foo.com/media/", "/media/".
+ADMIN_MEDIA_PREFIX = '/media/admin/'
+
+# Make this unique, and don't share it with anybody.
+SECRET_KEY = '4vykt!9qae5r6mj!$n3)5ep(qh1%pt_h3&66#j#x4qpvmk_!rp'
+
+DEFAULT_FROM_EMAIL = 'webmaster@orange.biolab.si'
+EMAIL_SUBJECT_PREFIX = '[Orange] '
+
+# List of callables that know how to import templates from various sources.
+TEMPLATE_LOADERS = (
+    'django.template.loaders.filesystem.Loader',
+    'django.template.loaders.app_directories.Loader',
+#     'django.template.loaders.eggs.Loader',
+)
+
+TEMPLATE_CONTEXT_PROCESSORS = (
+    'django.contrib.auth.context_processors.auth',
+    'django.core.context_processors.debug',
+    'django.core.context_processors.i18n',
+    'django.core.context_processors.request',
+    'django.core.context_processors.media',
+    'django.contrib.messages.context_processors.messages',
+    'cms.context_processors.media',
+)
+
+MIDDLEWARE_CLASSES = (
+    'django.middleware.common.CommonMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'cms.middleware.page.CurrentPageMiddleware',
+    'cms.middleware.user.CurrentUserMiddleware',
+    'cms.middleware.toolbar.ToolbarMiddleware',
+    'cms.middleware.media.PlaceholderMediaMiddleware',
+    'django.middleware.transaction.TransactionMiddleware',
+)
+
+ROOT_URLCONF = 'mainpage.urls'
+
+MESSAGE_STORAGE = 'django.contrib.messages.storage.session.SessionStorage'
+
+TEMPLATE_DIRS = (
+    # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
+    # Always use forward slashes, even on Windows.
+    # Don't forget to use absolute paths, not relative paths.
+    os.path.join(PROJECT_PATH, "templates"),
+)
+
+AUTHENTICATION_BACKENDS = (
+    'django.contrib.auth.backends.ModelBackend',
+)
+
+INSTALLED_APPS = (
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.sites',
+    'django.contrib.messages',
+    'django.contrib.admin',
+    'django.contrib.admindocs',
+    'cms',
+    'mptt',
+    'menus',
+    'south',
+    'appmedia',
+    'cms.plugins.text',
+)
+
+CMS_TEMPLATES = (
+    ('template_1.html', 'Template One'),
+    ('template_2.html', 'Template Two'),
+)
+
+CMS_PAGE_MEDIA_PATH = 'assets/'
+
+CMS_URL_OVERWRITE = False
+CMS_MENU_TITLE_OVERWRITE = False
+CMS_REDIRECTS = False
+CMS_FLAT_URLS = False
+CMS_SOFTROOT = True
+
+CMS_PERMISSION = False
+CMS_MODERATOR = False
+CMS_SHOW_START_DATE = False
+CMS_SHOW_END_DATE = False
+CMS_SEO_FIELDS = False
---
+++ urls.py
@@ -0,0 +1,17 @@
+from django.conf.urls.defaults import *
+from django.conf import settings
+
+from django.contrib import admin
+admin.autodiscover()
+
+urlpatterns = patterns('',
+    (r'^admin/doc/', include('django.contrib.admindocs.urls')),
+    (r'^admin/', include(admin.site.urls)),
+    url(r'^', include('cms.urls')),
+)
+
+if getattr(settings, 'DEBUG', None):
+    # Serve static files with Django when running in debug mode
+    urlpatterns = patterns('',
+        (r'^' + settings.MEDIA_URL.lstrip('/'), include('appmedia.urls')),
+    ) + urlpatterns
