d115f157ba4f3e8e5a8d021f75379003cff93486
Mitar <mitar.git@tnode.com>
2012-05-03 14:18:25 +0200

    Test.

--- mainpage/settings.py
+++ mainpage/settings.py
@@ -121,7 +121,7 @@ EMAIL_HOST = 'localhost'
 TEMPLATE_LOADERS = (
     'django.template.loaders.filesystem.Loader',
     'django.template.loaders.app_directories.Loader',
-#     'django.template.loaders.eggs.Loader',
+#    'django.template.loaders.eggs.Loader',
 )
 
 TEMPLATE_CONTEXT_PROCESSORS = (
