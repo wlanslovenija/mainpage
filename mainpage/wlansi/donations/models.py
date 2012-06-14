2116d6c23f9057c38146133fedac536bf38dc7d6
Mitar <mitar.git@tnode.com>
2012-06-14 02:44:33 +0200

    Limit minimal donation.

--- mainpage/wlansi/donations/models.py
+++ mainpage/wlansi/donations/models.py
@@ -1,9 +1,12 @@
+import decimal
+
+from django.core import validators
 from django.db import models
 from django.utils.translation import ugettext_lazy as _
 
 class Donation(models.Model):
     date = models.DateField(help_text=_("Date of donation on the account."))
-    amount = models.DecimalField(max_digits=12, decimal_places=2, help_text=_("In EUR, without any processing costs, final amount on the account."))
+    amount = models.DecimalField(max_digits=12, decimal_places=2, validators=[validators.MinValueValidator(decimal.Decimal('0.01'))], help_text=_("In EUR, without any processing costs, final amount on the account."))
     donor = models.CharField(max_length=255, blank=True, help_text=_("Leave blank if anonymous."))
     message = models.TextField(blank=True)
     internal_comment = models.TextField(blank=True, help_text=_("Internal comment, like donation source, circumstances, etc."))
