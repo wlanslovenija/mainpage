479f441a4419b4be11797ebe5907f3df9e04cf45
Mitar <mitar.git@tnode.com>
2012-06-17 21:12:08 +0200

    Removed unnecessary imports.

--- mainpage/wlansi/accounting/admin.py
+++ mainpage/wlansi/accounting/admin.py
@@ -2,7 +2,6 @@ from __future__ import absolute_import
 
 from django.conf import settings
 from django.contrib import admin
-from django.utils.translation import ugettext_lazy as _
 
 from . import models
 
--- mainpage/wlansi/donations/admin.py
+++ mainpage/wlansi/donations/admin.py
@@ -1,7 +1,6 @@
 from __future__ import absolute_import
 
 from django.contrib import admin
-from django.utils.translation import ugettext_lazy as _
 
 from . import models
 
