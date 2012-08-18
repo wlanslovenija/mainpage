6345cd37fcd8899bac1599f0a6f0a2bac82004d5
Luka Čehovin <luka.cehovin@gmail.com>
2012-08-19 01:34:26 +0200

    Panorama animation can be paused.

--- mainpage/wlansi/static/wlansi/js/panorama.js
+++ mainpage/wlansi/static/wlansi/js/panorama.js
@@ -1,10 +1,39 @@
 $.fn.panorama=function(o){
+
+	function getCookies() {
+		if (!document.cookie) return {};
+		var cookies = document.cookie.split(";"), x, y, i;
+		var object = {};
+		for (i = 0; i < cookies.length; i++) {
+			x = cookies[i].substr(0,cookies[i].indexOf("="));
+			y = cookies[i].substr(cookies[i].indexOf("=")+1);
+			x = x.replace(/^\s+|\s+$/g,"");
+			object[x] = unescape(y);
+		}
+		return object;
+	}
+
+	function setCookies(object) {
+		var cookies = [];
+		for (var x in object) {
+			if (object[x])
+				cookies.push(x + "=" + escape(object[x]));
+		}
+		document.cookie = cookies.join("; ");
+		console.log(cookies.join(";"));
+		console.log(document.cookie);
+	}
+
+	var cookies = getCookies();
+
 	var s = $.extend({images : [], transitionTime : 2000, panoramaTime : 60000}, o);
 
 	var c = this;
 	var cWidth = this.width();
 	var cHeight = this.height();
 	var cImage;
+
+	var cFlow = cookies["panorama_stop"] ? false : true;
 
 	c.css("overflow", "hidden");
 	c.css("position", "relative");
@@ -41,14 +70,23 @@ $.fn.panorama=function(o){
 			var nImage = $("<img />").attr({
 				src : imgurl, 
 				height : cHeight, 
-				style : "position : absolute; top: 0; opacity: 0;" 
+				style : "position : absolute; top: 0; opacity: 0; z-index: 0;" 
 			});
 			nImage.css("left", x1 + "px");
+			nImage.css("text-indent", x1 + "px");
 
 			c.append(nImage);
 			nImage.animate({opacity: 1}, { queue: false, duration: s.transitionTime} );
 
-			nImage.animate({left: x2 + "px"}, { queue: false, duration: s.panoramaTime, easing : 'linear'});
+			nImage.animate({"text-indent" : x2 + "px"}, { queue: false, 
+				duration: s.panoramaTime, 
+				easing : 'linear', step : function (now, prop) { 
+
+					if (cFlow)
+						$(prop.elem).css("left", now);
+					
+						//prop.start = prop.end = prop.now;
+				}});
 
 			if (cImage) {
 				cImage.animate({opacity: 0}, s.transitionTime, function() {
@@ -80,4 +118,19 @@ $.fn.panorama=function(o){
 	}
 
 	loadRandom();
+
+	var toggle = $('<a href="#">Pause</a>').addClass("toggle").attr("style", "z-index: 100; position: absolute; bottom: 10px; right: 10px; ");
+
+	if (!cFlow) toggle.addClass("play");
+
+	toggle.click(function (e) { 
+		cFlow = !cFlow;
+		$(this).html(cFlow ? "Pause" : "Play");
+		$(this).toggleClass("play");
+		e.preventDefault();
+		document.cookie = "panorama_stop=" + (cFlow ? "" : "1") + ";";
+	});
+
+	c.append(toggle);
+
 };
