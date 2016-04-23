093ff08578226e046064d8bb163d34dc8a0ad810
Jaka <github@breskev.com>
2016-04-23 08:41:57 +0200

    map: Moved to getJSON, fixed indent, static URLs for icons in template file

--- mainpage/wlansi/static/wlansi/js/map.js
+++ mainpage/wlansi/static/wlansi/js/map.js
@@ -4,14 +4,14 @@ var nodes = [];
 $(document).ready(function () {
     var icon = new google.maps.MarkerImage(
         // TODO: Very ugly, but it works
-        '/static/wlansi/images/gmap_node_up.png',
+        MAP_ICON_NODE_UP,
         new google.maps.Size(20, 32),
         new google.maps.Point(0, 0),
         new google.maps.Point(9, 30)
     );
     var shadow = new google.maps.MarkerImage(
         // TODO: Very ugly, but it works
-        '/static/wlansi/images/gmap_node_shadow.png',
+        MAP_ICON_NODE_SHADOW,
         new google.maps.Size(32, 32),
         new google.maps.Point(0, 0),
         new google.maps.Point(9, 30)
@@ -28,44 +28,45 @@ $(document).ready(function () {
     var map = new google.maps.Map(document.getElementById('map_canvas'), map_options);
 
     function updateStatus() {
-      var bounds = map.getBounds();
-      var visible_nodes = 0;
-      $.each(nodes, function (i, node) {
-        if (node.hasOwnProperty("l") && bounds.contains(new google.maps.LatLng(node.l[1], node.l[0]))) visible_nodes++;
-      });
-      var fmts = ngettext("%(visible_nodes)s active node visible (%(all_nodes)s all)", "%(visible_nodes)s active nodes visible (%(all_nodes)s all)", visible_nodes);
-      $('.map_status').text(interpolate(fmts, {
-         'visible_nodes': visible_nodes,
-         'all_nodes': nodes.length
-      }, true));
+        var bounds = map.getBounds();
+        var visible_nodes = 0;
+        $.each(nodes, function (i, node) {
+            if (node.hasOwnProperty("l") && bounds.contains(new google.maps.LatLng(node.l[1], node.l[0])))
+                visible_nodes++;
+        });
+        var fmts = ngettext("%(visible_nodes)s active node visible (%(all_nodes)s all)", "%(visible_nodes)s active nodes visible (%(all_nodes)s all)", visible_nodes);
+        $('.map_status').text(interpolate(fmts, {
+            'visible_nodes': visible_nodes,
+            'all_nodes': nodes.length
+        }, true));
     }
 
     function displayNodes() {
-      $.each(nodes, function (i, node) {
-        if (node.hasOwnProperty("l")) {
-          var marker = new google.maps.Marker({
-            'position': new google.maps.LatLng(node.l[1], node.l[0]),
-            'map': map,
-            'shadow': shadow,
-            'icon': icon,
-            'shape': shape,
-            'title': node.n
-          });
-          google.maps.event.addListener(marker, 'click', function () {
-            document.location = nodewatcher_url + "/node/" + node.i + "/";
-          });
-        }
-      });
-      updateStatus();
+        $.each(nodes, function (i, node) {
+            if (node.hasOwnProperty("l")) {
+                var marker = new google.maps.Marker({
+                    'position': new google.maps.LatLng(node.l[1], node.l[0]),
+                    'map': map,
+                    'shadow': shadow,
+                    'icon': icon,
+                    'shape': shape,
+                    'title': node.n
+                });
+                google.maps.event.addListener(marker, 'click', function () {
+                    document.location = nodewatcher_url + "/node/" + node.i + "/";
+                });
+            }
+        });
+        updateStatus();
     }
 
-    $.get(nodewatcher_url + "/api/v1/stream/?format=json&tags__module=topology&limit=1", function (data) {
-      var streamId = data.objects[0].id;
-      var latestTimestamp = Math.round(Date.parse(data.objects[0].latest_datapoint) / 1000);
-      $.getJSON(nodewatcher_url + "/api/v1/stream/" + streamId + "/?format=json&reverse=true&limit=1&start=" + latestTimestamp, function(x) {
-        nodes = x.datapoints[0].v.v;
-        displayNodes();
-      });
+    $.getJSON(nodewatcher_url + "/api/v1/stream/", {format: "json", tags__module: "topology", limit: 1}, function (data) {
+        var streamId = data.objects[0].id;
+        var latestTimestamp = Math.round(Date.parse(data.objects[0].latest_datapoint) / 1000);
+        $.getJSON(nodewatcher_url + "/api/v1/stream/" + streamId + "/", {format: "json", reverse: "true", limit: 1, start: latestTimestamp}, function(data) {
+            nodes = data.datapoints[0].v.v;
+            displayNodes();
+        });
     });
 
     google.maps.event.addListener(map, 'bounds_changed', updateStatus);
